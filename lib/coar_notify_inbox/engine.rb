e66fa32fadfdc2d813e490733867bcdbb4866a73
gyangupta10 <gyan@cottagelabs.com>
2025-11-11 14:12:33 +0530

    update engine config

--- lib/coar_notify_inbox/engine.rb
+++ lib/coar_notify_inbox/engine.rb
@@ -4,7 +4,7 @@ module CoarNotifyInbox
     config.generators.api_only = true
 
     initializer :append_migrations do |app|
-      if !app.root.to_s.match(root.to_s) && app.root.join('db/migrate').children.none? { |path| path.fnmatch?("*.coar_notify_inbox.rb") }
+      unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
